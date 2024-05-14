3f3e0751b5715462e8db4339523d251ec53d0248
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-14 14:30:28 +0200

    chore: add basic object

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php 
+    class Movie{
+        public $name;
+        public $director;
+        public $year;
+        public $description;
+        public $main_actors;
+        public $genre;
     
+        public function __construct($_name, $_director, $_year, $_description, $_main_actors, $_genre){
+            $this->name = $_name;
+            $this->director = $_director;
+            $this->year = $_year;
+            $this->description = $_description;
+            $this->main_actors = $_main_actors;
+            $this->genre = $_genre;
+        }
+    }
+    
+    $movie = new Movie("Titolo", "Director", 1984, "description", "main actor", "genre");
+    var_dump($movie);
 ?>
 
 <!DOCTYPE html>
